dfc67a5bf7037006e5b5076c8b529564aa5d972d
Liem Mazal <liemmazal5@gmail.com>
2022-05-09 08:48:59 +0300

    adding vehicle and get by license functions

--- controllers/Vhicles.js
+++ controllers/Vhicles.js
@@ -1,5 +1,6 @@
 const Vehicle = require('../models/vehicle_model')
 
+// Retrieve all of the available vehicles
 const getVeicles = async (req, res) => {
     try {
         Vehicles = await Vehicle.find()
@@ -9,6 +10,65 @@ const getVeicles = async (req, res) => {
     }
 }
 
+// Add a new vehicle to the vehicle's collection
+const addNewVehicle = async (req, res) => { 
+    const type = req.body.Type;
+    const modelName =req.body.ModelName;
+    const licenseNumber = req.body.LicenseNumber;
+    const availablEnergyPercentage = req.body.AvailablEnergyPercentage;
+    const maximumTirePressure = req.body.MaximumTirePressure;
+    const inflateTire=req.body.InflateTire;
+    const addEnergy =  req.body.AddEnergy;
+    const energySource =  req.body.EnergySource;
+    const wheels =  req.body.Wheels;
+
+    const vehicle = Vehicle({
+        Type:type,
+        ModelName:modelName,
+        LicenseNumber: licenseNumber,
+        AvailablEnergyPercentage :availablEnergyPercentage,
+        MaximumTirePressure :maximumTirePressure,
+        InflateTire:inflateTire,
+        AddEnergy: addEnergy,
+        EnergySource: energySource,
+        Wheels:wheels   
+       
+    })
+
+    const vehicle2 = await vehicle.save();
+    res.status(200).send(vehicle2)
+       
+}
+
+// Retrieve a single vehicle by license number
+const getVehicleByLicense = async (req, res) => {
+
+    try {       
+        const vehicle = await Vehicle.findOne({'LicenseNumber':req.params.LicenseNumber});
+        if(vehicle==null){
+            res.status(400).send({
+                'status': 'fail',
+                'error': err.message
+            })
+
+        }else{
+            res.status(200).send(vehicle)
+        }
+    } catch (err) {
+        res.status(400).send({
+            'status': 'fail',
+            'error': err.message
+        })
+    }
+}
+
+// Inflate vehicle tires to maximum pressure
+
+// Add energy (Refuel a vehicle or recharge) by license number
+
+
 module.exports = {
-    getVeicles
+    getVeicles,
+    addNewVehicle,
+    getVehicleByLicense
 }
--- routes/vehicles_route.js
+++ routes/vehicles_route.js
@@ -4,4 +4,8 @@ const Vehicle = require('../controllers/Vhicles')
 
 router.get('/getVeicles', Vehicle.getVeicles);
 
+router.post('/addNewVehicle', Vehicle.addNewVehicle);
+
+router.get('/getVehicleByLicense/:LicenseNumber', Vehicle.getVehicleByLicense);
+
 module.exports = router
